a2e72b403b007eff362b811a318fcad10bfdbe98
Mohamed Saleh <mohxbox36021@gmail.com>
2024-10-24 09:12:27 +0300

    added recipe schema

---
+++ models/recipe.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    instructions: {
+        type: String,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    ingredients: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
