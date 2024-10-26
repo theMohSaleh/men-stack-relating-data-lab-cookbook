83d9860b76199e48b039fbc66e2a94f00ed8d5e9
Mohamed Saleh <mohxbox36021@gmail.com>
2024-10-27 00:21:24 +0300

    css and recipe routes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -5,21 +5,42 @@ const router = express.Router();
 
 // GET - index page
 router.get("/", (req, res) => {
-    res.send("hddsfdsf");
+    res.send("test");
 });
 
 // GET - new recipe page
 router.get("/new", (req, res) => {
-    res.send("hddsfdsf");
+    res.send("test");
+    
 });
 
 // POST - create recipe
 router.post("/", async (req, res) => {
-
+    res.send("test");
+    
 });
 
 // GET - show page
 router.get("/:recipeId", (req, res) => {
+    res.send("test");
+    
+});
+
+// GET - edit page
+router.get("/:recipeId/edit", (req, res) => {
+    res.send("test");
+    
+});
+
+// PUT - update recipe
+router.put("/:recipeId", (req, res) => {
+    res.send("test");
+    
+});
+
+// PUT - update recipe
+router.delete("/:recipeId", (req, res) => {
+    res.send("test");
     
 });
 
